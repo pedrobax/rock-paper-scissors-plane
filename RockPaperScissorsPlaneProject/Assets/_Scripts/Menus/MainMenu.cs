c810a2cc1a0ba1441250f5538c3f7454e4d099ab
João Pedro Bax <joaopbax@gmail.com>
2023-04-10 14:49:38 -0300

    Changed LoadScene function to Asynchronous

--- RockPaperScissorsPlaneProject/Assets/_Scripts/Menus/MainMenu.cs
+++ RockPaperScissorsPlaneProject/Assets/_Scripts/Menus/MainMenu.cs
@@ -7,12 +7,23 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        StartCoroutine(LoadAsynchronously(1));
     }
 
     public void QuitGame()
     {
         Debug.Log("Quit!");
         Application.Quit();
+    }
+
+    IEnumerator LoadAsynchronously(int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+            Debug.Log(progress);
+            yield return null;
+        }
     }
 }
