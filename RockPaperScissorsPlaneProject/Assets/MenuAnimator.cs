98c6f9c70f8d3d78d1211c9e5ed4c8340302fc0e
João Pedro Bax <joaopbax@gmail.com>
2023-05-08 11:46:47 -0300

    Add grey fade to intro

--- RockPaperScissorsPlaneProject/Assets/MenuAnimator.cs
+++ RockPaperScissorsPlaneProject/Assets/MenuAnimator.cs
@@ -4,40 +4,37 @@ using UnityEngine;
 using DG.Tweening;
 using Unity.VisualScripting;
 using UnityEngine.UI;
+using TMPro;
 
 public class MenuAnimator : MonoBehaviour
 {
     public GameObject logo;
     public Image logoImage;
+    public Image fadeImage;
+    public TMP_Text playText;
+    public TMP_Text optionsText;
+    public TMP_Text quitText;
 
     private void Start()
     {
-        
-    }
-
-    private void Update()
-    {
-        if (Input.GetMouseButtonDown(1))
-        {
-            StartSequence();
-        }
-        if (Input.GetMouseButtonDown(0))
-        {
-           logoImage.DOFade(0, 0.1f);
-           logo.transform.DOScale(new Vector3(0.25f, 5, 1), 0.1f);
-            
-        }
+        StartSequence();
     }
 
     void StartSequence()
     {
         var logoStart = DOTween.Sequence();
+        logoStart.Append(fadeImage.transform.DOScale(new Vector3(40, 40, 40), 1));
         logoStart.Append(logo.transform.DOMove(new Vector3(1000, 8000, 0), 0.5f).From());
-        logoStart.Insert(0.25f, logo.transform.DOScale(new Vector3(2f, 0.2f, 1), 0.25f));
+        logoStart.Insert(1.25f, logo.transform.DOScale(new Vector3(2f, 0.2f, 1), 0.25f));
         logoStart.Append(logo.transform.DOScale(new Vector3(1, 1, 1), 0.25f));
-        logoStart.Insert(0, logoImage.DOFade(1, 0.3f));
 
+        logoStart.Insert(1, logoImage.DOFade(1, 0.3f));
+     
         logoStart.Append(logo.transform.DOScale(1.1f, 2).SetEase(Ease.InOutSine).SetLoops(-1, LoopType.Yoyo));
+
+        logoStart.Insert(4f, playText.DOFade(1, 0.5f));
+        logoStart.Insert(4f, optionsText.DOFade(1, 0.5f));
+        logoStart.Insert(4f, quitText.DOFade(1, 0.5f));
         //logoStart.Insert(0, logo.transform.DOPunchScale(new Vector3(-0.8f, 0.8f, 1), 0.25f, 1, 1));
         //logoStart.Insert(0.25f, logo.transform.DOPunchScale(new Vector3(0, -0.5f, 1), 0.25f, 1, 1));
     }
