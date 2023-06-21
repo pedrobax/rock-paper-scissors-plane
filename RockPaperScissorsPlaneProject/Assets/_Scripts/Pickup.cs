6c512fa1f491e48ac845b1c61d5cc35b184e86b6
João Pedro Bax <114126963+joaopbax@users.noreply.github.com>
2023-06-21 09:12:39 -0300

    Change extra lives now move downwards

--- RockPaperScissorsPlaneProject/Assets/_Scripts/Pickup.cs
+++ RockPaperScissorsPlaneProject/Assets/_Scripts/Pickup.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class Pickup : MonoBehaviour
 {
     [SerializeField] private Vector3 _rotation;
+    public float speed = 6f;
 
     void Update()
     {
         transform.Rotate(_rotation * Time.deltaTime); //rotates the object on the x, y, and z axis based on the rotation vector
+        transform.position += Vector3.back * speed * Time.deltaTime; //moves the object down based on the speed variable
     }
 
     private void OnTriggerEnter(Collider other)
