718fbb7507083418a65b95bae6c478a1a15c18e4
João Pedro Bax <114126963+joaopbax@users.noreply.github.com>
2023-06-23 12:54:39 -0300

    Add teleport to Menu background

--- RockPaperScissorsPlaneProject/Assets/_Scripts/Cloud.cs
+++ RockPaperScissorsPlaneProject/Assets/_Scripts/Cloud.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class Cloud : Movement
 {
-    [SerializeField] float teleportDistance = 200; //distance to teleport when hitting limit zone UNUSED
+    [SerializeField] float teleportDistance = 2000; //distance to teleport when hitting limit zone UNUSED
     [SerializeField] float speed; //linear speed of object
+    public bool teleportBack = false;
 
     Vector3 teleportDistanceVector = new Vector3(0, 0, 0);
 
@@ -17,6 +18,10 @@ public class Cloud : Movement
     // Update is called once per frame
     void Update()
     {
+        if(teleportBack && transform.position.z < -2000)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + teleportDistance);
+        }
         transform.Translate(0, 0, speed * -1 * Time.deltaTime); //moves object based on speed each frame
     }
 }
